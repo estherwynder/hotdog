d3264ed11b2b908029d780ac533e6c014fdb1547
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-10-12 11:40:45 +0200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  resources :chatrooms, only: %i[new create index show destroy]
+  resources :chatrooms, only: %i[index show]
   # root "articles#index"
 end
